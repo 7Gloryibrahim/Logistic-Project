81f5ae6c17bc015a57e2188b622f4578a8e263ee
Glory Ibrahim <ibrahimglory14@gmail.com>
2026-07-29 09:46:38 +0100

    My logistic project

---
+++ app/routes/home.tsx
@@ -0,0 +1,6 @@
+import {Page} from './page-one'
+
+export default function Home() {
+ return<Page/>;
+ 
+}
---
+++ app/routes/page-one.tsx
@@ -0,0 +1,192 @@
+import { useState,} from "react";
+import { Link } from "react-router";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { RiLinkedinFill } from "react-icons/ri";
+import { MdPhoneInTalk } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
+import { IoMenu } from "react-icons/io5";
+import { FaMagnifyingGlassChart } from "react-icons/fa6";
+import { MdArrowDropDown } from "react-icons/md";
+import { BsGeoAltFill } from "react-icons/bs";
+import { FaCircle } from "react-icons/fa";
+import { FaRegCircle } from "react-icons/fa";
+import { RiArrowRightWideFill } from "react-icons/ri";
+
+export function Page(){
+    const [search, setSearch] = useState(false)
+    return(
+        <div>
+             <div className="absolute z-50 w-full bg-[#062E39]  sm:block md:block">
+            {/*first Top */}
+            <div className="bg-[#2B4D57] hidden sm:flex sm:justify-between sm:px-7 md:pl-0 sm:ml-6 sm:mr-3  md:hidden  md:ml-6 lg:justify-between lg:pl-7 lg:flex   ">
+                {/* Left side */}
+                <div className="">
+                    <ul className=" sm:flex sm:gap-5  sm:py-3  ">
+                      
+                        <li className="flex gap-1 text-white  translate duration-300 hover:text-[#F75128]"><MdPhoneInTalk className="mt-1 text-[#F75128]"/>[+123 456 789]</li>
+                        <li className="flex text-white translate duration-300 hover:text-[#F75128]">
+                        < MdEmail className="mt-1 text-[#F75128]"/>
+                        <a href='mailto:support@logistra.com'>[support@logistra.com]</a></li>
+                    </ul>
+                </div>
+                {/*Right side */}
+                <div className="">
+                    <ul className="bg-[#F75128] py-3 px-10 flex sm:-mr-7 sm:w-80 sm:gap-3 sm:text-center md:gap-3 md:-mr-7 md:text-center lg:gap-3 lg:-mr-7 lg:text-center" style={{clipPath: "polygon( 15% 0%,100% 0%, 100% 100%,0% 100%)"}}>
+                        <li className="text-white  transition duration-300 hover:text-black"><Link to='/page-one'><FaFacebookF /></Link></li>
+                        <li className="border-r-2 border-amber-50"></li>
+                        <li className="text-white transition duration-300 hover:text-black">< FaXTwitter/></li>
+                         <li className="border-r-2 border-amber-50"></li>
+                        <li className="text-white transition duration-300 hover:text-black"><FaInstagram/> </li>
+                         <li className="border-r-2 border-amber-50"></li>
+                        <li className="text-white transition duration-300 hover:text-black"><RiLinkedinFill/></li>
+                         <li className="border-r-2 border-amber-50"></li>
+                        <li className="text-white transition duration-300 hover:text-black"
+                         onClick={() => setSearch(true)}
+                       >Search</li>
+                    </ul>
+                </div>
+            </div>
+
+            {/*Second Top */}
+
+                <div className="second-border flex bg-[#2B4D57] border-r-2 border-orange-600  mr-2 sm:border-r-0 sm:border-t-2 sm:ml-6 sm:h-20 sm:mr-3 md:mr-3 md:h-20 lg:mr-3 lg:h-20">
+                    <div className="bg-[#F75128] w-72 " style={{clipPath:"polygon(0 0, 100% 0, 90% 100%, 0 100%)"}}>
+                        <img src="/Truck1.png" alt="Truck" className="mt-4  sm:pl-1 sm:w " />
+                    </div>
+
+                    <div className=" flex justify-between w-full  font-bold">
+                        <ul className=" hidden  sm:flex sm:ml-4 sm:gap-6 mt-5 text-white items-center md:hidden lg:flex    ">
+                            <li className="Home flex">Home <MdArrowDropDown className="mt-1" />
+                            {/*First Drop Down */}
+                            <li className="Home-one">
+                            <li className="Drop-one">Header One</li>
+                            <li className="Drop-one">Header Two</li>
+                            <li className="Drop-one">Header Three</li>
+                            <li className="Fs-menu flex justify-between ">
+                            <li className="Drop-one  ">Header Styles</li>
+                             < RiArrowRightWideFill className="arrow-one mt-3 mr-3 text-2xl text-[#F75128]" />
+
+                            {/* Second Drop-down */}
+
+                            <li className="second-menu font-bold">
+                                <li className="home-head-one">Header One</li>
+                                <li className="home-head-one">Header Two</li>
+                                <li>Header Three</li>
+
+                            </li>
+
+                           </li>
+                            </li>
+                            </li>
+                            <li className="flex">About <MdArrowDropDown  className="mt-1"/></li>
+                            <li className="services flex">Services <MdArrowDropDown className="mt-1"/>
+                            <li className="service-drop-down ">
+                                <li>Services</li>
+                                <li>International Transport</li>
+                                <li>Local Track Transport</li>
+                                <li>Local Track Transport</li>
+                                <li>Fast Personal Delivery</li>
+                                <li>Safe Ocean Transport</li>
+                                 <li> Warehouse Facility</li>
+                               <li>Emergency Transport</li> 
+                            </li>
+                            </li>
+                            <li className="flex">Pages <MdArrowDropDown className="mt-1"/></li>
+                            <li className="flex">Shop <MdArrowDropDown  className="mt-1"/></li>
+                            <li className="flex">BLog <MdArrowDropDown  className="mt-1"/></li>
+                            <li>Contact</li>
+                        </ul>
+
+                            <div className="hidden  sm:hidden md:hidden lg:block">
+                        <button className="track-order border-2 border-double border-white text-white  p-3 h-20  md:block">TRACK ORDER</button>
+                        <span className="track1"></span>
+                        <span className="track2"></span>
+                        <span className="track3"></span>
+                        <span className="track4"></span>
+                    </div>
+                         {/*Mobile Icon */}
+                    <div className="absolute left-5/6  text-[#F75128] text-3xl font-bold  sm:hidden md:block md:mt-6 md:left-5/6  lg:hidden ">
+                        <IoMenu />
+                    </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            {/*Hero Background-Image */}
+            <div>
+                <div style={{backgroundImage:"url('/homebg.jpg')", backgroundPosition:"center", backgroundAttachment:"fixed",backgroundSize:"cover", overflow:"hidden"}} className="relative overflow-hidden h-96 sm:h-screen md:h-screen lg:h-screen ">
+               
+              
+                <div>
+                    <img src="/hero-globe.png" alt="Globe" className="absolute   left-1/2 -translate-x-1/2 z-40 top-28 sm:mx-auto md:top-36    lg:top-44"/>
+{/*Goe location icons */}
+                    <div className=" text-wrap top-44 w-fit  ">
+
+                            
+                    </div>
+
+                </div>
+
+                  {/* Hero Overlay */}
+                <div className="absolute bg-black/50 inset-0 h-96 sm:h-screen md:h-screen lg:h-screen"></div>
+               
+                
+               
+                    
+
+            </div>
+             </div>
+            <div
+    onClick={() => setSearch(false)}
+    className={`
+        fixed
+        top-0
+        right-0
+        bottom-0
+        left-0
+
+        bg-[#F75128]/50
+
+        flex
+        justify-center
+        items-start
+
+        pt-64
+        z-50
+        transition-transform
+        duration-700
+        ease-in-out
+
+        ${
+            search
+                ? "translate-y-0"
+                : "-translate-y-full"
+        }
+    `}
+>
+
+    <div
+        onClick={(e) => e.stopPropagation()}
+        className="w-full flex justify-center">
+
+        <input
+            type="text,number"
+            placeholder="Search..."
+            className=" py-5 px-6 relative w-72 rounded-l-2xl border-2 border-[#F75128] bg-white outline-0 text-black"
+        />
+        <label className=" bg-[#F75128] rounded-r-2xl w-20">
+            <FaMagnifyingGlassChart className="text-2xl text-white h-16 ml-4 text-center   " />
+        </label>
+        
+    </div>
+
+            </div>
+
+        </div>
+    )
+}
